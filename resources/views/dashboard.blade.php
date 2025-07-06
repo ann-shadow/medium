2cc011e8789e38305b19cb055c8e188ed41d91a2
annane <ann.wolfshadow@gmail.com>
2025-07-06 20:52:39 +0300

    post card replacement

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,8 +21,19 @@
                 </div>
             </div>
 
-            <div class="text-gray-900 mt-8 flex flex-wrap justify-center gap-4">
+            <div class="text-gray-900 mt-8">
                 @foreach($posts as $post)
+
+                    <a href="#" class="mb-4 flex flex-col w-full bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:items-stretch md:justify-between  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+
+                        <div class="flex flex-col justify-between p-4 leading-normal">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $post->title }}</h5>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ Str::words($post->content, 20) }}</p>
+                        </div>
+                        <img class="object-cover w-full rounded h-48 md:h-auto md:w-48 md:rounded-none" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
+                    </a>
+
+                @endforeach
 
                     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mb-8">
                         <a href="#">
@@ -44,7 +55,7 @@
                     </div>
 
 
-                @endforeach
+                {{--@endforeach--}}
             </div>
 
         </div>
